a49a14b2309838a0a2f8bb01f8bf0cd67cb0e77d
bilguunchinzorigEPFL <bilguun.chinzorig@nafora.ch>
2018-11-13 13:57:29 +0100

    Added other configurations

---
+++ src/main/java/FCHJGenerator.java
@@ -0,0 +1,53 @@
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by beku on 11/13/2018.
+ */
+public class FCHJGenerator implements DataGenerator {
+
+    DataSet dataSet;
+    int trainSize;
+
+    public FCHJGenerator(String filename,double trainPercent,int shift_amount){
+        ArrayList<Double> spread=new ArrayList<>();
+        ArrayList<LocalDate> dates=new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] elems = line.split(",");
+                if(elems.length==2){
+                    dates.add(LocalDate.parse(elems[0]));
+                    spread.add(Double.valueOf(elems[1]));
+                }
+            }
+            reader.close();
+        } catch (Exception e){
+            String tmp="";
+        }
+        trainSize=(int)(trainPercent*(spread.size()-shift_amount));
+        double[] data=new double[spread.size()-shift_amount];
+        double[] dataShifted=new double[spread.size()-shift_amount];
+        for (int i = 0; i < spread.size()-shift_amount; i++) {
+            data[i]=spread.get(i);
+            dataShifted[i]=spread.get(i+shift_amount);
+        }
+        dataSet=new org.nd4j.linalg.dataset.DataSet(Nd4j.create(data),Nd4j.create(dataShifted));
+    }
+
+    public DataSet getTrainDataSet() {
+        return dataSet.getRange(0,trainSize);
+    }
+
+    public DataSet getTestDataSet() {
+        return dataSet.getRange(trainSize,dataSet.numExamples());
+    }
+}
---
+++ src/main/java/NetworkConfiguration.java
@@ -0,0 +1,56 @@
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+/**
+ * Created by beku on 11/13/2018.
+ */
+public enum NetworkConfiguration {
+    Test,
+    Regression;
+
+    MultiLayerConfiguration getConfig(){
+        switch (this){
+            case Test:
+                return testConfig();
+            case Regression:
+                return regressionConfig();
+            default:
+                return null;
+        }
+    }
+
+    private MultiLayerConfiguration testConfig(){
+        MultiLayerConfiguration conf=new NeuralNetConfiguration.Builder()
+            .updater(new Sgd(0.1))
+            .list()
+            .layer(0, new DenseLayer.Builder()
+                .activation(Activation.SIGMOID)
+                .nIn(2).nOut(4).build())
+            .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .activation(Activation.SOFTMAX)
+                .nIn(4).nOut(2).build())
+            .backprop(true)
+            .build();
+        return conf;
+    }
+
+    private MultiLayerConfiguration regressionConfig(){
+        MultiLayerConfiguration conf=new NeuralNetConfiguration.Builder()
+            .updater(new Sgd(0.1))
+            .list()
+            .layer(0, new DenseLayer.Builder()
+                .activation(Activation.SIGMOID)
+                .nIn(2).nOut(4).build())
+            .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .activation(Activation.SOFTMAX)
+                .nIn(4).nOut(1).build())
+            .backprop(true)
+            .build();
+        return conf;
+    }
+}

bfbfec361f864cbdca414863eaad542b5246b01e
bilguunchinzorigEPFL <bilguun.chinzorig@nafora.ch>
2018-11-16 14:30:48 +0100

    Added other configurations

--- src/main/java/NetworkConfiguration.java
+++ src/main/java/NetworkConfiguration.java
@@ -1,6 +1,7 @@
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.LSTM;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.learning.config.Sgd;
@@ -11,7 +12,8 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
  */
 public enum NetworkConfiguration {
     Test,
-    Regression;
+    Regression,
+    LSTM;
 
     MultiLayerConfiguration getConfig(){
         switch (this){
@@ -19,6 +21,8 @@ public enum NetworkConfiguration {
                 return testConfig();
             case Regression:
                 return regressionConfig();
+            case LSTM:
+                return lstmConfig();
             default:
                 return null;
         }
@@ -44,6 +48,21 @@ public enum NetworkConfiguration {
             .updater(new Sgd(0.1))
             .list()
             .layer(0, new DenseLayer.Builder()
+                .activation(Activation.SIGMOID)
+                .nIn(2).nOut(4).build())
+            .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .activation(Activation.SOFTMAX)
+                .nIn(4).nOut(1).build())
+            .backprop(true)
+            .build();
+        return conf;
+    }
+
+    private MultiLayerConfiguration lstmConfig(){
+        MultiLayerConfiguration conf=new NeuralNetConfiguration.Builder()
+            .updater(new Sgd(0.1))
+            .list()
+            .layer(0, new LSTM.Builder()
                 .activation(Activation.SIGMOID)
                 .nIn(2).nOut(4).build())
             .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
